ae35643c4d58d58822cbae9833dddd857810fdb1
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-12-02 14:12:58 +0200

    Component life cycle method, component will mount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll';
 
 
 class App extends React.Component {
@@ -10,10 +10,20 @@ class App extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                return response.json();  
+            })
+            .then(users => {
+                this.setState({robots: users})
+            })
+    }
+
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value});
     }
@@ -26,7 +36,9 @@ class App extends React.Component {
             <div className='tc'>
                 <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
             </div>
         );
     }
