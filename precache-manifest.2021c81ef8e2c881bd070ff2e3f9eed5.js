f4c39b4efb28609634d9d4e79ca0676a6777dba6
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-12-02 15:57:38 +0200

    Updates

---
+++ precache-manifest.2021c81ef8e2c881bd070ff2e3f9eed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e3f0f12822e8c38750216a694d8508",
+    "url": "/robotsapp/index.html"
+  },
+  {
+    "revision": "cbba607220da8924d4ff",
+    "url": "/robotsapp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7562eb546e700747e259",
+    "url": "/robotsapp/static/css/main.2cf127fa.chunk.css"
+  },
+  {
+    "revision": "cbba607220da8924d4ff",
+    "url": "/robotsapp/static/js/2.d245a95a.chunk.js"
+  },
+  {
+    "revision": "7562eb546e700747e259",
+    "url": "/robotsapp/static/js/main.bf3ae2d8.chunk.js"
+  },
+  {
+    "revision": "b551483b3d3c2b4c5e05",
+    "url": "/robotsapp/static/js/runtime-main.3c1973cf.js"
+  }
+]);
