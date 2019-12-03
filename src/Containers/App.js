afc69ddb0c39f25f9eac151d9a55568178e84947
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-12-03 13:11:50 +0200

    implemented connecting reducers to app.js

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import './App.css';
 import Scroll from '../Components/Scroll';
 import ErrorBoundry from '../Components/ErrorBoundry';
+import {setSearchField} from '../actions';
 
+const mapStateToProps = state => {
+    return {
+        //searchfields tha twe will return is going to come from the state.search robots. searchfield
+        searchField: state.searchField
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        //just a prop
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 class App extends React.Component {
     //app now owns state so we can change it only here
     constructor(props){
         super(props)
         this.state = {
-            robots: [],
-            searchfield: ''
+            robots: []
         }
     }
     componentDidMount(){
@@ -24,20 +37,17 @@ class App extends React.Component {
                 this.setState({robots: users})
             })
     }
-
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value});
-    }
     render(){
-        const {robots, searchfield} = this.state;
+        const {robots} = this.state;
+        const {searchField, onSearchChange} = this.props;
         //should give the value of what is in the search box
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
         return (
             <div className='tc'>
                 <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundry>
                         <CardList robots={filteredRobots}/>
@@ -47,5 +57,5 @@ class App extends React.Component {
         );
     }
 }
-
-export default App;
+//using connect, connect is going to run, return another function and returns a function with app as an argument
+export default connect(mapStateToProps, mapDispatchToProps)(App);
